6148bae6c7c9fb2fd8a101a36d9fa078e5259857
Qexat <contact@qexat.com>
2022-08-30 17:42:57 +0200

    🖌️ Tried another code design for main program

--- src/__main__.py
+++ src/__main__.py
@@ -1,50 +1,77 @@
 #!/usr/bin/env python3
 
-# pyright: reportUnusedCallResult=false
+from __future__ import annotations
 
+import sys
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
+from typing import NoReturn
 
 from braincum import Error, Interpreter, panic
 
 
-def init() -> Namespace:
-    """
-    Initialize command line argument parser
+class Program:
+    @staticmethod
+    def _parse_args(args: list[str]) -> Namespace:
+        """
+        Helper method to parse command line args.
 
-    Returns:
-        Namespace: parsed args
-    """
+        Args:
+            args (list[str]): args to parse
 
-    parser = ArgumentParser()
-    parser.add_argument("file")
+        Returns:
+            Namespace: argparse special object that stores parsed args.
+        """
 
-    return parser.parse_args()
+        parser = ArgumentParser()
+        parser.add_argument("file")  # type: ignore
 
+        return parser.parse_args(args)
 
-def main() -> int:
-    """
-    Main program
+    def __init__(self: Program, *, args: list[str] | None = None) -> None:
+        """
+        Parses command line args and stores the result.
 
-    Raises:
-        FileNotFoundError: if the provided file was not found
+        Note: The argument `args` exists for testing purposes and should not be used otherwise.
 
-    Returns:
-        int: exit code
-    """
+        Args:
+            args (list[str] | None, optional): args to be parsed. Defaults to None.
+        """
 
-    args = init()
+        self.args = self._parse_args(args or sys.argv)
 
-    file_path = Path(args.file).resolve()
+    def run(self: Program) -> int:
+        """
+        Runs the program and returns an exit code.
 
-    if not file_path.exists():
-        panic(Error.FileNotFound, file_path)
+        Returns:
+            int: exit code.
+        """
 
-    content = file_path.read_text()
-    i = Interpreter(content)
+        file_path = Path(self.args.file).resolve()
 
-    return 0
+        if not file_path.exists():
+            panic(Error.FileNotFound, file_path)
+
+        content = file_path.read_text(encoding="utf-8")
+
+        try:
+            i = Interpreter(content)
+        except:
+            return 1
+        else:
+            return 0
+
+    def execute(self: Program) -> NoReturn:
+        """
+        Runs the program and exits.
+
+        Raises:
+            SystemExit: when the program terminates.
+        """
+
+        raise SystemExit(self.run())
 
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    Program().execute()
